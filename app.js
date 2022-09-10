2e992e77cdc1f2b7ab5f2753c075b19e04710cb1
CalixOtu <98580506+CalixOtu@users.noreply.github.com>
2022-09-10 01:38:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 
 function SendMail() {
-    emailjs.send("service_vdmjvud","template_ocjsvoc",{
+    emailjs.send("service_vdmjvud","template_ocjsvoc", {
     from_name: document.getElementById('name').value,
     email_id: document.getElementById('mail').value,
+    subject: document.getElementById('subject').value,
     message: document.getElementById('message').value,
-});
+}).then(alert("Message Successfully Sent!"));
 }
